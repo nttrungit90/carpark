f434a6813485f62346ea5f7a61ebd2d4a284fc31
trung <nttrungit90@gmail.com>
2024-03-08 00:29:57 +0700

    Last change

--- src/main/java/com/wego/carpark/dto/NearestCarParkDto.java
+++ src/main/java/com/wego/carpark/dto/NearestCarParkDto.java
@@ -1,14 +1,26 @@
 package com.wego.carpark.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class NearestCarParkDto {
+    @JsonIgnore
     private String carParkNo;
+
     private String address;
+
     private Double latitude;
+
     private Double longitude;
+
+    @JsonProperty("total_lots")
     private Integer totalLots;
+
+    @JsonProperty("available_lots")
     private Integer availableLots;
+
+    @JsonIgnore
     private Double distance;
 }
